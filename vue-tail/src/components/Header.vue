839b7843dcb5bb1555bed4ed31fefc58a7aea37d
Nur Fauzan Najib <nurfauzannajib@gmail.com>
2021-07-29 09:48:44 +0700

    header

---
+++ vue-tail/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<header class="text-center bg-blue-400 p-4 mb-4">
+  <div class="text-white text-3xl md:text-5xl">Covid-19 Tracker</div>
+  <p class="text-white">
+    API by
+    <a class="text-blue-600" href="https://covid19api.com" target="_blank">covid19api.com</a>
+  </p>
+</header>
+  
+</template>
+
+<script>
+export default {
+  name: "Header",
+ 
+};
+</script>
+
+
